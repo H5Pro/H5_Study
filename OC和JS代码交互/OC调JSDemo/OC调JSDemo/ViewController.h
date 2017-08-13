62a49dd30c36894a2e55e0052368e4df23edd13e
付金亮 <738623786@qq.com>
2017-08-13 21:10:47 +0800

    OC和JS互调

---
+++ OC和JS代码交互/OC调JSDemo/OC调JSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC调JSDemo
+//
+//  Created by fjl on 2017/8/7.
+//  Copyright © 2017年 fjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OC和JS代码交互/OC调JSDemo/OC调JSDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OC调JSDemo
+//
+//  Created by fjl on 2017/8/7.
+//  Copyright © 2017年 fjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
