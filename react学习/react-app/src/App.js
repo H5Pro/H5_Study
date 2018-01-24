dd8d16d5ea7596c34497c70effdf644a9fb580ee
付金亮 <738623786@qq.com>
2018-01-24 17:38:07 +0800

    react学习-项目

--- react学习/react-app/src/App.js
+++ react学习/react-app/src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FirstComponent from './components/first-component';
-import SecondComponent from './components/second-component'
-import Clock from './components/clock'
+import ReactRouter from './components/ReactRouter'
+import ReactStart from './components/ReactStart'
+import ReactRedux from './components/ReactRedux'
 class App extends Component {
   render() {
     return (
+
             <div className="app">
               <header className="app-header">
-                <img src={logo} classyName="app-logo" alt="logo" />
+                {/*<img src={logo} classyName="app-logo" alt="logo" />*/}
                 <h1 className="app-title">Welcome to React</h1>
               </header>
+              {/*
               <p className="app-intro">
                 To get started, edit <code>src/app.js</code> and save to reload.
               </p>
-              <FirstComponent name="111"></FirstComponent>
-              <SecondComponent ></SecondComponent>
-              <Clock/>
+              */}
+              {/*<ReactStart/>*/}
+              {/*<ReactRouter/>*/}
+              <ReactRedux/>
             </div>
     );
   }
