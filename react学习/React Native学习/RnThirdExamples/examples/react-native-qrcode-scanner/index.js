020bcf8e1b520ab3f69b6d1d2b6fd39fb2863d1e
fjl <738623786@qq.com>
2018-06-30 12:33:10 +0800

    iOS车市通过

--- react学习/React Native学习/RnThirdExamples/examples/react-native-qrcode-scanner/index.js
+++ react学习/React Native学习/RnThirdExamples/examples/react-native-qrcode-scanner/index.js
@@ -18,25 +18,16 @@ import QRCodeScanner from 'react-native-qrcode-scanner';
 
 class ScanScreen extends Component {
   onSuccess(e) {
-    Linking
-      .openURL(e.data)
-      .catch(err => console.error('An error occured', err));
+    alert(e.data)
+    // Linking
+    //   .openURL(e.data)
+    //   .catch(err => console.error('An error occured', err));
   }
 
   render() {
     return (
       <QRCodeScanner
         onRead={this.onSuccess.bind(this)}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
       />
     );
   }
