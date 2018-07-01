97afcbd28530ecea19dbd219f5eaf1061a5db174
fjl <738623786@qq.com>
2018-07-01 16:34:42 +0800

    ios 运行成功

--- smartBarcodeTest/App.js
+++ smartBarcodeTest/App.js
@@ -11,7 +11,7 @@ import {
   Text,
   View
 } from 'react-native';
-
+import SmartBarCodeExample from './SmartBarCodeExample'
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -23,17 +23,7 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <SmartBarCodeExample />
     );
   }
 }
