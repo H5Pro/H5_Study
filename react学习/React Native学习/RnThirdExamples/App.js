134200b66b57526cc0fe37f18cac0e208e8aa96c
fjl <738623786@qq.com>
2018-07-02 08:32:29 +0800

    完成二维码生成功能和复制粘贴功能

--- react学习/React Native学习/RnThirdExamples/App.js
+++ react学习/React Native学习/RnThirdExamples/App.js
@@ -4,7 +4,8 @@
  * @flow
  */
 import React, { Component } from 'react';
-import {View} from 'react-native'
+import {View, Clipboard, Button,  StyleSheet, TextInput} from 'react-native'
+import QRCode from 'react-native-qrcode';
 // import ReactNativeSearchList from './examples/react-native-search-list'
 // import ReactNativeSearchBox from './examples/react-native-search-box'
 // import ReactNativeSearchBar from './examples/react-native-search-bar'
@@ -16,12 +17,55 @@ import {View} from 'react-native'
 // import ReactNativeTabView from './examples/react-native-tab-view'
 import BmQrcodeScanner from './components/BmQrcodeScanner' // 扫码功能
 export default class App extends Component{
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: 'http://facebook.github.io/react-native/',
+    }
+  }
+
   render() {
     return (
-      <View style={{marginTop: 20, backgroundColor: '#f2f2f2', flex: 1}}>
-        <BmQrcodeScanner />
+      <View style={{backgroundColor: '#f2f2f2', flex: 1}}>
+        {/*<BmQrcodeScanner />*/}
+        {/*<Button     // 粘贴板功能
+          title={'粘贴'}
+          onPress={() => {
+            console.log(111)
+            Clipboard.setString('hello world')
+        }}></Button>*/}
+        <View style={styles.container}>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({text: text})}
+            value={this.state.text}
+          />
+          <QRCode
+            value={this.state.text}
+            size={200}
+            bgColor='purple'
+            fgColor='white'/>
+        </View>
       </View>
     );
   }
 }
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    borderRadius: 5,
+    padding: 5,
+  }
+});
