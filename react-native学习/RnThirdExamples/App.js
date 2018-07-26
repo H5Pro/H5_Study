d72ac14ea5d87bd5c1ed97727fcfb71a67749ae1
fjl <738623786@qq.com>
2018-07-26 17:08:37 +0800

    解决问题

--- react-native学习/RnThirdExamples/App.js
+++ react-native学习/RnThirdExamples/App.js
@@ -4,7 +4,6 @@
  * @flow
  */
 import React, { Component } from 'react';
-import {Text, StyleSheet} from 'react-native'
 // import ReactNativeSearchList from './examples/react-native-search-list'
 // import ReactNativeSearchBox from './examples/react-native-search-box'
 // import ReactNativeSearchBar from './examples/react-native-search-bar'
@@ -17,34 +16,11 @@ import {Text, StyleSheet} from 'react-native'
 // import QRCodeExample from './examples/react-native-qrcode'
 // import SmartBarcodeExample from './examples/react-native-smart-barcode'
 // import Web3 from './examples/web3js'
-// import PayPasswordExample from './examples/react-native-pay-password'
+import PayPasswordExample from './examples/react-native-pay-password'
 export default class App extends Component{
   render() {
     return (
-    <View style={styles.container} >
-      <Text style={styles.iconStyle}></Text>
-      <Text style={styles.iconStyle}></Text>
-      <Text style={styles.iconStyle}></Text>
-      <Text style={styles.iconStyle}></Text>
-      <Text style={styles.iconStyle}></Text>
-    </View>
+      <PayPasswordExample></PayPasswordExample>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container:{
-    backgroundColor:'white',
-    width:width-44,
-    height: 36,
-    marginTop: 2,
-    flexDirection:'row',
-    alignItems: 'center',
-    justifyContent: 'space-around'
-  },
-  iconStyle: {
-    color: 'red',
-    fontFamily:'iconfont',
-    fontSize: 30
-  }
-})
