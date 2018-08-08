8eaab2c1720efc5832f1efd505b8c971689e61be
付金亮 <738623786@qq.com>
2018-08-08 09:57:24 +0800

    完成企业数字钱包说明页的确认过程

--- react-native学习/RnThirdExamples/App.js
+++ react-native学习/RnThirdExamples/App.js
@@ -6,7 +6,7 @@
 import React, { Component } from 'react'
 // import ReactNativeSearchList from './examples/react-native-search-list'
 // import ReactNativeSearchBox from './examples/react-native-search-box'
-// import ReactNativeSearchBar from './examples/react-native-search-bar'
+import ReactNativeSearchBar from './examples/react-native-search-bar'
 // import BmSearchPickerDemo from './myExample/BmSearchPickerDemo'
 // import PopoverExample from './examples/react-native-popover'
 // import ModalPopoverExample from './examples/react-native-modal-popover'
@@ -17,11 +17,11 @@ import React, { Component } from 'react'
 // import SmartBarcodeExample from './examples/react-native-smart-barcode'
 // import Web3 from './examples/web3js'
 // import PayPasswordExample from './examples/react-native-pay-password'
-import BmLoaderDemo from './myExample/BmLoaderDemo'
+// import BmLoaderDemo from './myExample/BmLoaderDemo'
 class App extends Component {
   render () {
     return (
-      <BmLoaderDemo />
+      <ReactNativeSearchBar />
     )
   }
 }
