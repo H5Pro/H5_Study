797af9c4389bf8888b9308545e68fa6ebb13c70a
付金亮 <jinliang.fu@gselab.org>
2018-11-19 16:10:49 +0800

    ios原生项目集成cordova完成

--- 框架/cordova学习/cordova-first/platforms/ios/platform_www/cordova_plugins.js
+++ 框架/cordova学习/cordova-first/platforms/ios/platform_www/cordova_plugins.js
@@ -1,38 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-camera.Camera",
-    "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
-    "pluginId": "cordova-plugin-camera",
-    "clobbers": [
-      "Camera"
-    ]
-  },
-  {
-    "id": "cordova-plugin-camera.CameraPopoverOptions",
-    "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
-    "pluginId": "cordova-plugin-camera",
-    "clobbers": [
-      "CameraPopoverOptions"
-    ]
-  },
-  {
-    "id": "cordova-plugin-camera.camera",
-    "file": "plugins/cordova-plugin-camera/www/Camera.js",
-    "pluginId": "cordova-plugin-camera",
-    "clobbers": [
-      "navigator.camera"
-    ]
-  },
-  {
-    "id": "cordova-plugin-camera.CameraPopoverHandle",
-    "file": "plugins/cordova-plugin-camera/www/ios/CameraPopoverHandle.js",
-    "pluginId": "cordova-plugin-camera",
-    "clobbers": [
-      "CameraPopoverHandle"
-    ]
-  },
-  {
     "id": "cordova-plugin-wkwebview-engine.ios-wkwebview-exec",
     "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview-exec.js",
     "pluginId": "cordova-plugin-wkwebview-engine",
@@ -46,14 +14,6 @@ module.exports = [
     "pluginId": "cordova-plugin-wkwebview-engine",
     "clobbers": [
       "window.WkWebView"
-    ]
-  },
-  {
-    "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
-    "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
-    "pluginId": "phonegap-plugin-barcodescanner",
-    "clobbers": [
-      "cordova.plugins.barcodeScanner"
     ]
   },
   {
@@ -101,9 +61,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-camera": "4.0.3",
   "cordova-plugin-wkwebview-engine": "1.1.4",
-  "phonegap-plugin-barcodescanner": "8.0.0",
   "cordova-plugin-add-swift-support": "1.7.2",
   "cordova-plugin-qrscanner": "2.6.0",
   "cordova-plugin-geolocation": "4.0.1"
