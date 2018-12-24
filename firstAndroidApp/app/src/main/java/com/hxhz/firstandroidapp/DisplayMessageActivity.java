062efdf2c59606cc57cf6edd63bc8cfcf218d9df
付金亮 <jinliang.fu@gselab.org>
2018-12-24 14:56:27 +0800

    解决问题

--- firstAndroidApp/app/src/main/java/com/hxhz/firstandroidapp/DisplayMessageActivity.java
+++ firstAndroidApp/app/src/main/java/com/hxhz/firstandroidapp/DisplayMessageActivity.java
@@ -26,13 +26,13 @@ public class DisplayMessageActivity extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
         webSettings.setJavaScriptEnabled(true);
-        //其他细节操作
-//        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+        // 其他细节操作
+        // webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
         webSettings.setAllowFileAccess(true); //设置可以访问文件
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
         webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
         webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
-
+        
         webSettings.setDomStorageEnabled(true);
         webSettings.setAppCacheMaxSize(1024*1024*8);
         String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
