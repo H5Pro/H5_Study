ea67f01dbfcf21894122724247ce754d70553a76
付金亮 <jinliang.fu@gselab.org>
2019-01-29 17:16:31 +0800

    webGL学习

--- ITO(物联网)/h5 3D/webGL-learn/初探/index.js
+++ ITO(物联网)/h5 3D/webGL-learn/初探/index.js
@@ -1,3 +1,17 @@
 /**
  * Created by fjl on 2019/1/24
  */
+function main() {
+  const canvas = document.querySelector("#glcanvas");
+  const gl = canvas.getContext("webgl");
+  if (!gl) {
+    alert("Unable to initialize webGL. Your browser or machine may not support it.")
+    return
+  }
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  main()
+})
