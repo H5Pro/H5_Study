5e728afd5f211e08a1d609c9d6294f424277426b
fujinliang <jinliang.fu@gselab.org>
2019-06-03 15:32:52 +0800

    webpack学习 - 管理输出

---
+++ 框架/webpack学习/webpack-demo/webpack.config.js
@@ -0,0 +1,42 @@
+/**
+ * Created by fjl on 2019/5/30
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+};
